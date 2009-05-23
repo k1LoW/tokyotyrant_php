6dd826cdfa5bcd2a669e113ed0a069b4820b8b28
k1LoW <k1lowxb@gmail.com>
2009-05-23 14:48:00 +0900

    fix doc.

--- testcase.php
+++ testcase.php
@@ -18,61 +18,70 @@ $count_key = 'count';
 $extname = 'echo';
 $error = null;
 
+//TokyoTyrant_RDB->open()
 $getdata = $tt->open('dummy', 1978);
-
 assert(!$getdata);
-
 $tt->open('localhost', 1978, 1000);
-
 assert(strlen($tt->stat()) > 1);
 
+//initialize
 assert($tt->vanish() === true);
 
+//TokyoTyrant_RDB->put()
 assert($tt->put($key, $data) === true);
+
+//TokyoTyrant_RDB->get()
 $getdata = $tt->get($key);
 assert($getdata === $data);
 
+//TokyoTyrant_RDB->putkeep()
 assert($tt->putkeep($key, $data . 'keep') === false);
 $getdata = $tt->get($key);
 assert($getdata === $data);
 
+//initialize
 $tt->out($key);
 
+//TokyoTyrant_RDB->putkeep()
 assert($tt->putkeep($key, $data . 'keep') === true);
 
+//TokyoTyrant_RDB->putcat()
 $getdata = $tt->get($key);
 assert($getdata === $data . 'keep');
-
 assert($tt->put($key, $data) === true);
 $getdata = $tt->get($key);
-
 assert($getdata === $data);
 assert($tt->putcat($key, $data) === true);
-
 $getdata = $tt->get($key);
 assert($getdata === $data . $data);
 
+//TokyoTyrant_RDB->out()
 assert($tt->put($key, $data) === true);
-
-
 assert($tt->out($key) === true);
 $getdata = $tt->get($key);
 assert($getdata === false);
 
+//TokyoTyrant_RDB->mget()
 assert($tt->put($key, $data));
 assert($tt->put($key2, $data2));
 assert($tt->mget(array($key, $key2)) === 2);
+
+//TokyoTyrant_RDB->fwmkeys()
 assert(count($tt->fwmkeys('key', 2)) === 2);
+
+//TokyoTyrant_RDB->vsiz()
 assert($tt->vsiz($key) === strlen($data));
+
+//TokyoTyrant_RDB->iterinit(), TokyoTyrant_RDB->iternext()
 assert($tt->vanish() === true);
 assert($tt->iterinit() === true);
 assert($tt->iternext() === false);
-
 assert($tt->put($key, $data));
 assert($tt->iterinit() === true);
 assert($tt->iternext() === $key);
 assert($tt->iternext() === false);
 
+//TokyoTyrant_RDB->addint()
 assert($tt->addint($count_key, 1) === 1);
 assert($tt->addint($count_key, 2) === 3);
 assert($tt->addint($count_key, -2) === 1);
@@ -96,14 +105,17 @@ assert($tt->sync() === true);
 assert(!is_array($tt->size()));
 assert(!is_array($tt->rnum()));
 
-assert($tt->copy('/tmp/test.net_tokyotyrant.db') === true);
-assert(file_exists('/tmp/test.net_tokyotyrant.db') === true);
+//TokyoTyrant_RDB->copy()
+assert($tt->copy('/tmp/test.tokyotyrant_php.db') === true);
+assert(file_exists('/tmp/test.tokyotyrant_php.db') === true);
 
 assert($tt->vanish() === true);
+
+//TokyoTyrant_RDB->stat()
 assert(strlen($tt->stat()) > 1);
 
 assert($tt->optimize('') === true);
-assert($tt->copy('/tmp/test.net_tokyotyrant.db') === true);
+assert($tt->copy('/tmp/test.tokyotyrant_php.db') === true);
 
 $tt->vanish();
 $tt->close();
@@ -118,25 +130,29 @@ $data = array("OS" => "Ubuntu", "DBM" => "TT/TC", "Language" => "PHP", "Web Serv
 $data2 = array("OS" => "CentOS", "DBM" => "PostgreSQL", "Language" => "Ruby", "Web Server" => "Apache/passenger", "Memory" => "2000000000");
 $error = null;
 
+//TokyoTyrant_RDBTBL->open()
 $getdata = $tb->open('dummy', 1980);
-
 assert(!$getdata);
-
 $tb->open('localhost', 1980, 1000);
 assert(strlen($tb->stat()) > 1);
 
 assert($tb->vanish() === true);
-assert($tb->setindex('name', TokyoTyrant_RDBTBL::ITLEXICAL));
 
+//TokyoTyrant_RDBTBL->setindex()
+assert($tb->setindex('OS', TokyoTyrant_RDBTBL::ITLEXICAL));
+
+//TokyoTyrant_RDBTBL->genuid()
 $pkey = $tb->genuid();
 assert($pkey !== -1);
 assert($tb->put($pkey, $data));
 assert($tb->get($pkey) === $data);
 
+//TokyoTyrant_RDBTBL->get()
 $pkey = $tb->genuid();
 assert($tb->putkeep($pkey, $data));
 assert($tb->get($pkey) === $data);
 
+//TokyoTyrant_RDBTBL->out()
 assert($tb->out($pkey) === true);
 assert($tb->get($pkey) === false);
 
@@ -145,10 +161,10 @@ $tb->vanish();
 $pkey = $tb->genuid();
 assert($tb->putkeep($pkey, $data));
 assert($tb->get($pkey) === $data);
-
 $pkey = $tb->genuid();
 assert($tb->putkeep($pkey, $data2));
 
+//TokyoTyrant_RDBQRY::QCSTRINC, TokyoTyrant_RDBQRY::QOSTRDESC
 $tq = new TokyoTyrant_RDBQRY($tb);
 $tq->addcond("DBM", TokyoTyrant_RDBQRY::QCSTRINC, "TT");
 $tq->setorder("Memory", TokyoTyrant_RDBQRY::QOSTRDESC);
@@ -160,6 +176,7 @@ $result = $tq->search();
 
 assert($tb->get($result[0]) === $data);
 
+//TokyoTyrant_RDBQRY::QOSTRDESC
 $tq = new TokyoTyrant_RDBQRY($tb);
 $tq->setorder("Memory", TokyoTyrant_RDBQRY::QOSTRDESC);
 assert($tq->searchcount() == 2);
